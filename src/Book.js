0301e8806e415990ff65fb9cdff86daa6c96cb54
KOUMEDZRO Oréliau <oreliauk@gmail.com>
2018-08-27 23:43:44 +0200

    final project

---
+++ src/Book.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import ShelfChanger from './ShelfChanger'
+
+class Book extends Component {
+
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  }
+
+  render() {
+    const { book, books, changeShelf } = this.props
+
+    // add fallbacks for missing cover images and title
+    const coverImg = book.imageLinks
+    const title = book.title
+
+    return (
+          <li>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  style={{ backgroundImage: `url(${coverImg})`}}>
+                </div>
+                <ShelfChanger
+                  book={ book }
+                  books={ books }
+                  changeShelf={changeShelf }
+                />
+              </div>
+              <div className="book-title">{ title }</div>
+              { /* Check for authors and render each on separate line if exist*/
+                book.authors && book.authors.map((author, index) => (
+                  <div className="book-authors" key={index}>{author}</div>
+              ))}
+            </div>
+          </li>
+    )
+  }
+
+}
+
+export default Book
